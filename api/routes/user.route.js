d264739c9e7b392ebd8c69c72510e01fc546e9c9
aman <amanpachauria@gmail.com>
2023-12-24 13:52:54 +0530

    add delete functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { text, updateUser } from '../controllers/user.controller.js';
+import { text, updateUser, deleteUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utills/verifyUser.js';
 
 const router = express.Router();
 
 router.get("/text", text);
 router.post('/update/:id', verifyToken, updateUser );
+router.delete('/delete/:id', verifyToken, deleteUser );
 
 export default router;
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -7,8 +7,6 @@ import OAuth from "../components/OAuth";
 export default function SignIn() {
 
     const [formData, setFormData] = useState({});
-    // const [error, setError] = useState(null);
-    // const [loading, setLoading] = useState(false);
     const { loading, error } = useSelector( (state) => state.user );
     const navigate = useNavigate();
     const dispatch = useDispatch();
@@ -26,7 +24,6 @@ export default function SignIn() {
     const handleSubmit = async (e) => {
           e.preventDefault();
           try {
-            // setLoading(true);
             dispatch(signInStart());
             const res = await fetch("/api/auth/signin", {
               method: "POST",
@@ -38,19 +35,13 @@ export default function SignIn() {
             const data = await res.json()
             console.log(data);
             if( data.success === false ){
-                //  setLoading(false);
-                //  setError(data.message);
                 dispatch(signInFailure(data.message));
                  return;
             }
 
-            // setLoading(false);
-            // setError(null);
             dispatch(signInSuccess(data));
             navigate("/");
           } catch (error) {
-            //  setLoading(false);
-            //  setError(error.message);
             dispatch(signInFailure(error.message));
           }
     };
@@ -58,7 +49,7 @@ export default function SignIn() {
 
   return (
     <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center font-semibold my-7">Sign Ip</h1>
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
 
         <input
