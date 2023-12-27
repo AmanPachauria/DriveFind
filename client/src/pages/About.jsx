9d91e8a9af5ca3645099fc447c633107a835459a
aman <amanpachauria@gmail.com>
2023-12-27 07:38:28 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About DriveFind</h1>
+      <p className='mb-4 text-slate-700'>DriveFind is a premier online platform dedicated to connecting car buyers and sellers, facilitating seamless transactions in the automotive market. Our platform is designed to simplify the process of buying and selling vehicles, making it efficient and enjoyable for our users.</p>
+      <p className='mb-4 text-slate-700'>
+        At DriveFind, we strive to revolutionize the car buying and selling experience. Our mission is to provide a user-friendly interface, comprehensive listings, and valuable insights to empower users in making informed decisions. Whether you're in search of your dream car or looking to sell your vehicle hassle-free, DriveFind is here to meet your automotive needs.
+      </p>
+      <p className='mb-4 text-slate-700'>Our dedicated team is passionate about enhancing the automotive marketplace. We combine cutting-edge technology with a commitment to customer satisfaction, ensuring that our users have a smooth and secure experience on DriveFind. We believe that finding the perfect car or selling your vehicle should be a straightforward and enjoyable journey, and we're committed to making that vision a reality for all our users.</p>
+    </div>
+  );
 }
